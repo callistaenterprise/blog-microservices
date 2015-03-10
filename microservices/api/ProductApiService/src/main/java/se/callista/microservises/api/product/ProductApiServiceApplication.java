321cf4aaa92b261b75173155534840e19d907e52
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2015-03-10 16:41:49 +0100

    Turned the product-api-service into a OAuth2 resource (deadeasy!)

--- microservices/api/ProductApiService/src/main/java/se/callista/microservises/api/product/ProductApiServiceApplication.java
+++ microservices/api/ProductApiService/src/main/java/se/callista/microservises/api/product/ProductApiServiceApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableDiscoveryClient
+@EnableOAuth2Resource
 public class ProductApiServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApiServiceApplication.class, args);
+        System.err.println("### PAS-1");
     }
 }
