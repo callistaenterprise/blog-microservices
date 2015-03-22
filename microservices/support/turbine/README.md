f36be8e8e44b5b63b29b5ec3d441ae5f44bd8e4d
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2015-03-22 20:50:30 +0100

    Added Turbine

---
+++ microservices/support/turbine/README.md
@@ -0,0 +1,3 @@
+turbine
+=======
+After running, use this url in the hystrix dashboard: http://localhost:8989/turbine.stream?cluster=CUSTOMERS
---
+++ microservices/support/turbine/src/main/java/turbine/TurbineApplication.java
@@ -0,0 +1,22 @@
+package turbine;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.amqp.EnableTurbineAmqp;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Spencer Gibb
+ * @author Dave Syer
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableTurbineAmqp
+@EnableDiscoveryClient
+public class TurbineApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(TurbineApplication.class).run(args);
+    }
+}
