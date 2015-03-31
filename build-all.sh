586c2dae6c37be8422bc1e2345ddaea4db06bc32
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2015-03-31 17:39:48 +0200

    Branch B1 for the first implementation blog regarding microservices.

--- build-all.sh
+++ build-all.sh
@@ -4,13 +4,9 @@ cd microservices/core/product-service;                ./gradlew clean publishToM
 cd microservices/core/recommendation-service;         ./gradlew clean publishToMavenLocal; cd -
 cd microservices/core/review-service;                 ./gradlew clean publishToMavenLocal; cd -
 cd microservices/composite/product-composite-service; ./gradlew clean build; cd -
-cd microservices/api/product-api-service;             ./gradlew clean build; cd -
 
-cd microservices/support/auth-server;                 ./gradlew clean build; cd -
 cd microservices/support/config-server;               ./gradlew clean build; cd -
 cd microservices/support/discovery-server;            ./gradlew clean build; cd -
-cd microservices/support/edge-server;                 ./gradlew clean build; cd -
 cd microservices/support/monitor-dashboard;           ./gradlew clean build; cd -
 cd microservices/support/turbine;                     ./gradlew clean build; cd -
 
-cd webapp/ui;                                         mvn clean package; cd -
