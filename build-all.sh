b6f25b5dce04fa099d5467284eba07544c19ab3a
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2015-04-02 13:45:55 +0200

    Start on blog build microservices, part 1, added a simple edge server

--- build-all.sh
+++ build-all.sh
@@ -7,6 +7,7 @@ cd microservices/composite/product-composite-service; ./gradlew clean build; cd 
 
 cd microservices/support/config-server;               ./gradlew clean build; cd -
 cd microservices/support/discovery-server;            ./gradlew clean build; cd -
+cd microservices/support/edge-server;                 ./gradlew clean build; cd -
 cd microservices/support/monitor-dashboard;           ./gradlew clean build; cd -
 cd microservices/support/turbine;                     ./gradlew clean build; cd -
 
