e38b0ffb3852967443d1609889ead0bb39ed6ea6
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2015-05-15 09:30:32 +0200

    Automated creation of Docker images using the gradle-build-scripts

--- build-all.sh
+++ build-all.sh
@@ -2,18 +2,19 @@
 
 cd util;                                              ./gradlew clean publishToMavenLocal; cd -
 
-cd microservices/core/product-service;                ./gradlew clean publishToMavenLocal build; cd -
-cd microservices/core/recommendation-service;         ./gradlew clean publishToMavenLocal build; cd -
-cd microservices/core/review-service;                 ./gradlew clean publishToMavenLocal build; cd -
-cd microservices/composite/product-composite-service; ./gradlew clean build; cd -
-cd microservices/api/product-api-service;             ./gradlew clean build; cd -
+cd microservices/core/product-service;                ./gradlew clean publishToMavenLocal build distDocker; cd -
+cd microservices/core/recommendation-service;         ./gradlew clean publishToMavenLocal build distDocker; cd -
+cd microservices/core/review-service;                 ./gradlew clean publishToMavenLocal build distDocker; cd -
+cd microservices/composite/product-composite-service; ./gradlew clean build distDocker; cd -
+cd microservices/api/product-api-service;             ./gradlew clean build distDocker; cd -
 
-cd microservices/support/auth-server;                 ./gradlew clean build; cd -
+cd microservices/support/auth-server;                 ./gradlew clean build distDocker; cd -
+# cd microservices/support/config-server;               ./gradlew clean build distDocker; cd -
 cd microservices/support/config-server;               ./gradlew clean build; cd -
-cd microservices/support/discovery-server;            ./gradlew clean build; cd -
-cd microservices/support/edge-server;                 ./gradlew clean build; cd -
-cd microservices/support/monitor-dashboard;           ./gradlew clean build; cd -
-cd microservices/support/turbine;                     ./gradlew clean build; cd -
+cd microservices/support/discovery-server;            ./gradlew clean build distDocker; cd -
+cd microservices/support/edge-server;                 ./gradlew clean build distDocker; cd -
+cd microservices/support/monitor-dashboard;           ./gradlew clean build distDocker; cd -
+cd microservices/support/turbine;                     ./gradlew clean build distDocker; cd -
 
 cd webapp/ui;                                         mvn clean package; cd -
 
