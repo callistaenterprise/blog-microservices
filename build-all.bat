716660b4ed4a183e737d882bd1c6a96daf64a137
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2015-06-03 13:48:53 +0200

    Added support for Windows bat-files for Blog #1 on branch B1 (to start with)

---
+++ build-all.bat
@@ -0,0 +1,7 @@
+pushd microservices\core\product-service &                call gradlew clean publishToMavenLocal & popd
+pushd microservices\core\recommendation-service &         call gradlew clean publishToMavenLocal & popd
+pushd microservices\core\review-service &                 call gradlew clean publishToMavenLocal & popd
+pushd microservices\composite\product-composite-service & call gradlew clean build & popd
+
+pushd microservices\support\discovery-server &            call gradlew clean build & popd
+pushd microservices\support\edge-server &                 call gradlew clean build & popd
