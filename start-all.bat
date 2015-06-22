0af384bcb6323f78b87a50fb1343941c6f39af64
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2015-06-22 17:12:11 +0200

    Added monitor-dashboard and turbine to the windows bat-files

--- start-all.bat
+++ start-all.bat
@@ -1,5 +1,9 @@
+# Use Windows Services to ensure that the Rabbitmq service is started!
+
 start /D microservices\support\discovery-server             gradlew bootRun
 start /D microservices\support\edge-server                  gradlew bootRun
+start /D microservices\support\monitor-dashboard            gradlew bootRun
+start /D microservices\support\turbine                      gradlew bootRun
 
 start /D microservices\core\product-service                 gradlew bootRun
 start /D microservices\core\recommendation-service          gradlew bootRun
