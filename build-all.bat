1183b444b2181a316a747f87b27bb0dd0081fd91
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2015-06-23 11:41:49 +0200

    Added auth-server and api-service the windows bat-files for blog #3 about OAuth

--- build-all.bat
+++ build-all.bat
@@ -2,7 +2,9 @@ pushd microservices\core\product-service &                call gradlew clean pub
 pushd microservices\core\recommendation-service &         call gradlew clean publishToMavenLocal & popd
 pushd microservices\core\review-service &                 call gradlew clean publishToMavenLocal & popd
 pushd microservices\composite\product-composite-service & call gradlew clean build & popd
+pushd microservices\api\product-api-service &             call gradlew clean build & popd
 
+pushd microservices\support\auth-server &                 call gradlew clean build & popd
 pushd microservices\support\discovery-server &            call gradlew clean build & popd
 pushd microservices\support\edge-server &                 call gradlew clean build & popd
 pushd microservices\support\monitor-dashboard &           call gradlew clean build & popd
--- start-all.bat
+++ start-all.bat
@@ -1,5 +1,6 @@
 # Use Windows Services to ensure that the Rabbitmq service is started!
 
+start /D microservices\support\auth-server                  gradlew bootRun
 start /D microservices\support\discovery-server             gradlew bootRun
 start /D microservices\support\edge-server                  gradlew bootRun
 start /D microservices\support\monitor-dashboard            gradlew bootRun
@@ -9,3 +10,4 @@ start /D microservices\core\product-service                 gradlew bootRun
 start /D microservices\core\recommendation-service          gradlew bootRun
 start /D microservices\core\review-service                  gradlew bootRun
 start /D microservices\composite\product-composite-service  gradlew bootRun 
+start /D microservices\api\product-api-service              gradlew bootRun 
