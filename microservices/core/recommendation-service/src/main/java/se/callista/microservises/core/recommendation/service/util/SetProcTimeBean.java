bc55bc93f2e8c0371b8c7bfe4b4377844486e467
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2015-12-29 14:39:15 +0100

    First usage of a config server in recommendation server

--- microservices/core/recommendation-service/src/main/java/se/callista/microservises/core/recommendation/service/util/SetProcTimeBean.java
+++ microservices/core/recommendation-service/src/main/java/se/callista/microservises/core/recommendation/service/util/SetProcTimeBean.java
@@ -3,21 +3,33 @@ package se.callista.microservises.core.recommendation.service.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 /**
  * TODO: Extract to a common util-lib
  */
+@RefreshScope
 @Component
 public class SetProcTimeBean {
     
     private static final Logger LOG = LoggerFactory.getLogger(SetProcTimeBean.class);
 
-    @Value("${service.defaultMinMs}")
     private int minMs;
 
-    @Value("${service.defaultMaxMs}")
     private int maxMs;
+
+    @Value("${service.defaultMinMs:0}")
+    public void setMinMs(int minMs) {
+        LOG.info("Set min response time to {} ms.", this.minMs);
+        this.minMs = minMs;
+    }
+
+    @Value("${service.defaultMaxMs:0}")
+    public void setMaxMs(int maxMs) {
+        LOG.info("Set max response time to {} ms.", this.maxMs);
+        this.maxMs = maxMs;
+    }
 
     public void setDefaultProcessingTime(int minMs, int maxMs) {
 
@@ -35,6 +47,7 @@ public class SetProcTimeBean {
 
     public int calculateProcessingTime() {
         int processingTimeMs = minMs + (int) (Math.random() * (maxMs - minMs));
+        LOG.info("Response time interval: {} - {} ms.", minMs, maxMs);
         LOG.debug("Return calculated processing time: {} ms", processingTimeMs);
         return processingTimeMs;
     }
---
+++ microservices/support/config-server/src/test/java/se/callista/microservises/support/ConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package se.callista.microservises.support;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ConfigServerApplication.class)
+public class ConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
