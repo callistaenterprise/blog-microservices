e3afaf6e78e4efad9c7bd4ab6de6e73b4fd7dde6
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2016-01-01 12:37:58 +0100

    Second commit for upgrading to SPRING CLOUD 1.1.n, updated build and property files for better log-levels and usage of the config server for properties

--- microservices/support/config-server/src/main/java/se/callista/microservises/support/ConfigServerApplication.java
+++ microservices/support/config-server/src/main/java/se/callista/microservises/support/ConfigServerApplication.java
@@ -2,9 +2,11 @@ package se.callista.microservises.support;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
