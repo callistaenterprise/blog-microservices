f5c5925d7f5656a4bf3f11de37db8701d2d3073e
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2016-01-02 14:42:54 +0100

    Added gen-wrapper.sh and used it to upgrade gradle version used by gradlew for all projects

---
+++ gen-wrappers.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd util;                                              ./gradlew wrapper; cd -
+
+cd microservices/core/product-service;                ./gradlew wrapper; cd -
+cd microservices/core/recommendation-service;         ./gradlew wrapper; cd -
+cd microservices/core/review-service;                 ./gradlew wrapper; cd -
+cd microservices/composite/product-composite-service; ./gradlew wrapper; cd -
+cd microservices/api/product-api-service;             ./gradlew wrapper; cd -
+
+cd microservices/support/auth-server;                 ./gradlew wrapper; cd -
+cd microservices/support/config-server;               ./gradlew wrapper; cd -
+cd microservices/support/discovery-server;            ./gradlew wrapper; cd -
+cd microservices/support/edge-server;                 ./gradlew wrapper; cd -
+cd microservices/support/monitor-dashboard;           ./gradlew wrapper; cd -
+cd microservices/support/turbine;                     ./gradlew wrapper; cd -
