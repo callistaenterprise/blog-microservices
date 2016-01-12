41188960494af2b64225cb6ef7688268bd04076d
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2016-01-12 11:22:35 +0100

    Improved test-all.sh. Displayed the curl commands + retries the API call typically waiting until everything is settled in the landscape after a startup

--- test-all.sh
+++ test-all.sh
@@ -10,7 +10,7 @@
 
 function testUrl() {
     url=$@
-    if curl $url -s -f -o /dev/null
+    if curl $url -ks -f -o /dev/null
     then
           echo "Ok"
           return 0
@@ -50,6 +50,43 @@ function waitForServices() {
     waitForService $host:8761/eureka/apps/composite-service
 }
 
+
+function testAPI() {
+    url=$@
+#    url="https://$host/api/product/123 -H \"Authorization: Bearer $TOKEN\""
+#    echo "Test API: $url"
+#    if curl "$url" -ki -f
+#    if curl $url -ks -f -o /dev/null
+    if curl -ks https://$host/api/product/123 -H "Authorization: Bearer $TOKEN" -f -o /dev/null
+    then
+          echo "Ok"
+          return 0
+    else
+          echo -n "fail"
+          return 1
+    fi;
+}
+
+function waitForAPI() {
+
+    url=$@
+    echo -n "Wait for API: $url... "
+    n=0
+    until testAPI $url
+    do
+        ((n++))
+        if [[ $n == 20 ]]
+        then
+            echo " Give up"
+            exit 1
+        else
+            sleep 6
+            echo -n ", retry #$n "
+        fi
+    done
+
+}
+
 if [[ $@ == *"start"* ]]
 then
     echo "Restarting the test environment..."
@@ -59,12 +96,20 @@ fi
 
 waitForServices
 
-echo -n "Tries to get an Access Token... "
-TOKEN=`curl -ks https://acme:acmesecret@$host:9999/uaa/oauth/token   -d grant_type=password  -d client_id=acme  -d scope=webshop  -d username=user  -d password=password | jq -r .access_token`
-echo "Got: $TOKEN"
+echo ''
+echo "Get an OAuth Access Token:"
+echo "$ curl -ks https://acme:acmesecret@$host:9999/uaa/oauth/token -d grant_type=password -d client_id=acme -d scope=webshop -d username=user -d password=password | jq ."
+OAUTH_RESPOSE=`curl -ks https://acme:acmesecret@$host:9999/uaa/oauth/token -d grant_type=password -d client_id=acme -d scope=webshop -d username=user -d password=password`
+echo $OAUTH_RESPOSE | jq .
+TOKEN=`echo $OAUTH_RESPOSE | jq -r .access_token`
+#TOKEN=`curl -ks https://acme:acmesecret@$host:9999/uaa/oauth/token   -d grant_type=password  -d client_id=acme  -d scope=webshop  -d username=user  -d password=password | jq -r .access_token`
+echo "ACCESS TOKEN: $TOKEN"
 
-echo -n "Tries to call the API with the Access Token... "
-curl https://$host/api/product/123   -H  "Authorization: Bearer $TOKEN" -ks | jq .
+echo ''
+echo "Call API with Access Token... "
+waitForAPI
+echo "$ curl -ks https://$host/api/product/123 -H \"Authorization: Bearer $TOKEN\" | jq ."
+curl -ks https://$host/api/product/123 -H  "Authorization: Bearer $TOKEN" | jq .
 
 if [[ $@ == *"stop"* ]]
 then
