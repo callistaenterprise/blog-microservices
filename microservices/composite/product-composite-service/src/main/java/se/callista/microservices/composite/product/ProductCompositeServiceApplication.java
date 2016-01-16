871c90379a021058c4c332c3352d1dfb09e3e988
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2016-01-16 21:59:52 +0100

    CADEC-2016: Only use Config-server and EventBus from review-service for faster startup

--- microservices/composite/product-composite-service/src/main/java/se/callista/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/composite/product-composite-service/src/main/java/se/callista/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -3,14 +3,10 @@ package se.callista.microservices.composite.product;
 import com.netflix.hystrix.strategy.HystrixPlugins;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import se.callista.microservices.util.MDCHystrixConcurrencyStrategy;
@@ -45,15 +41,15 @@ public class ProductCompositeServiceApplication {
 //        LOG.info("Registration of a StatsD Metrics Reporter done!");
 //    }
 
-    @Value("${app.rabbitmq.host:localhost}")
-    String rabbitMqHost;
-
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        LOG.info("Create RabbitMqCF for host: {}", rabbitMqHost);
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitMqHost);
-        return connectionFactory;
-    }
+//    @Value("${app.rabbitmq.host:localhost}")
+//    String rabbitMqHost;
+//
+//    @Bean
+//    public ConnectionFactory connectionFactory() {
+//        LOG.info("Create RabbitMqCF for host: {}", rabbitMqHost);
+//        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitMqHost);
+//        return connectionFactory;
+//    }
 
     public static void main(String[] args) {
         LOG.info("Register MDCHystrixConcurrencyStrategy");
--- microservices/core/product-service/src/main/java/se/callista/microservices/core/product/ProductServiceApplication.java
+++ microservices/core/product-service/src/main/java/se/callista/microservices/core/product/ProductServiceApplication.java
@@ -2,13 +2,9 @@ package se.callista.microservices.core.product;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -31,15 +27,15 @@ public class ProductServiceApplication {
 //        LOG.info("Registration of a StatsD Metrics Reporter done!");
 //    }
 
-    @Value("${app.rabbitmq.host:localhost}")
-    String rabbitMqHost;
-
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        LOG.info("Create RabbitMqCF for host: {}", rabbitMqHost);
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitMqHost);
-        return connectionFactory;
-    }
+//    @Value("${app.rabbitmq.host:localhost}")
+//    String rabbitMqHost;
+//
+//    @Bean
+//    public ConnectionFactory connectionFactory() {
+//        LOG.info("Create RabbitMqCF for host: {}", rabbitMqHost);
+//        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitMqHost);
+//        return connectionFactory;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
