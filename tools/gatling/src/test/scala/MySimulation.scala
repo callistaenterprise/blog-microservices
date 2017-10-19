28f35f9262368afbf8b5fa882824fd7c35d05b12
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2017-10-19 12:24:24 +0200

    SPRING5: 1. Added Zipkin to docker compose 2. Added support for starting microservices using mert 3. Added loadtest based on Gatling

---
+++ tools/gatling/src/test/scala/MySimulation.scala
@@ -0,0 +1,57 @@
+import com.typesafe.config.ConfigFactory
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef.{http, jsonPath, status}
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.duration._
+
+class MySimulation extends Simulation {
+
+  // val noOfUsers  = 3000 // ASYNC. Ger ca 750 req/s och < 10 ms svarstid
+  // val noOfUsers  = 1500 // SYNC. Ger ca 400 req/s och lite drygt 10 ms svarstid
+  // val rampUpTime = 120 seconds
+  // val testTime   = 180 seconds
+  // val minWaitMs  = 3000 milliseconds
+  // val maxWaitMs  = 5000 milliseconds
+  val noOfUsers  = 500 // SYNC. Ger ca 400 req/s och lite drygt 10 ms svarstid
+  val rampUpTime = 12 seconds
+  val testTime   = 18 seconds
+  val minWaitMs  = 300 milliseconds
+  val maxWaitMs  = 500 milliseconds
+
+  private val log = LoggerFactory.getLogger("composite.CompositeTestScenario")
+
+  val conf = ConfigFactory.load
+
+  def httpHostname = conf.getString("callista.http.hostname")
+
+  val baseUrl = "http://" + httpHostname + ":8084"
+  // val requestUrl = "/async/5"
+  val requestUrl = "/5"
+
+  val httpConf = http
+    .baseURL(baseUrl)
+    .acceptHeader("application/json;charset=UTF-8;q=0.9,*/*;q=0.8")
+    .warmUp(baseUrl + "/")
+
+  val myScenario: ChainBuilder =
+    exec(http("Composite Test")
+      .get(requestUrl)
+      .check(status.is(200))
+      .check(jsonPath("$").saveAs("result"))
+    )
+    .exec(session => {
+      log.debug( "Result: {}", session( "result" ).as[String] )
+      session
+    })
+
+  setUp(
+    scenario("Test Scenario")
+      .during(testTime) {
+        exec(myScenario).pause(minWaitMs, maxWaitMs)
+      }
+      .inject(rampUsers(noOfUsers) over(rampUpTime))
+      .protocols(httpConf)
+  )
+}
